8c2735f51e4dcae442269da52a14d7399622c76c
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-02-19 10:40:17 -0300

    ✨ feat: footer

--- dnc-arq-rafael-de-sa/src/components/Footer/Footer.jsx
+++ dnc-arq-rafael-de-sa/src/components/Footer/Footer.jsx
@@ -16,11 +16,12 @@ function Footer() {
       <div className="container">
         <div className="d-flex jc-space-between">
           <div className="footer-logo-col">
+            <img src={Logo} className="footer-logo" />
             <p className="gray-1-color">
               A escola que prepara você para as profissões em alta no mercado de
               trabalho.
             </p>
-            <div className="d-flex social-link">
+            <div className="d-flex social-links">
               <a href="https://google.com" target="_blank">
                 <img src={FacebookIcon} alt="" />
               </a>
@@ -34,6 +35,41 @@ function Footer() {
                 <img src={InstagramIcon} alt="" />
               </a>
             </div>
+          </div>
+          <div className="d-flex">
+            <div className="footer-col">
+              <h3>Pages</h3>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About us</Link>
+                </li>
+                <li>
+                  <Link to="/projects">Projects</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-col">
+              <h3>Contact</h3>
+              <p className="gray-1-color">
+                R. Justino Cobra, 61 – Vila Ema | São José dos Campos – SP | CEP
+                12243-030
+              </p>
+              <p className="gray-1-color">suporte@escoladnc.com.br</p>
+              <p className="gray-1-color">(19) 99187-4342</p>
+            </div>
+          </div>
+        </div>
+        <div className="d-flex jc-space-between footer-copy">
+          <p className="gray-1-color">Copyright © DNC - 2024</p>
+          <div className="langs-area d-flex">
+            <img src={BrazilIcon} height="29px" />
+            <img src={UsaIcon} height="29px" />
           </div>
         </div>
       </div>
