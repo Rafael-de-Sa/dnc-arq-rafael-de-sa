56e99cfbef9a7ce39d160388782f38377bc85e5e
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-02-20 20:45:59 -0300

    ✨ feat: ProjectsList

--- dnc-arq-rafael-de-sa/src/pages/Home.jsx
+++ dnc-arq-rafael-de-sa/src/pages/Home.jsx
@@ -3,16 +3,15 @@ import Button from "../components/Button/Button";
 import Hero from "../components/Hero/Hero";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import ProjectsList from "../components/ProjectsList/ProjectsList";
 
 function Home() {
   return (
     <>
       <Header />
-      <h1>Home</h1>
-      <p>Subtitle</p>
-      <Button buttonStyle="primary">Olá</Button>
       <div className="container">
         <Hero />
+        <ProjectsList />
       </div>
       <Footer />
     </>
