56f061df8647eba002798ed7e75bb54b5ffbdf9d
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-02-27 10:53:15 -0300

    ✨ feat: loading projects provided from API

--- dnc-arq-rafael-de-sa/src/components/ProjectsList/ProjectsList.jsx
+++ dnc-arq-rafael-de-sa/src/components/ProjectsList/ProjectsList.jsx
@@ -1,10 +1,28 @@
+import { useState, useEffect } from "react";
 import "./ProjectsList.css";
 
 //ASSETS
 import LikedFilled from "../../assets/like-filled.svg";
 import Like from "../../assets/like.svg";
 
+//UTILS
+import { getApiData } from "../../services/apiServices";
+
 function ProjectsList() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const projectsResponse = await getApiData("projects");
+        setProjects(projectsResponse);
+      } catch {
+        setProjects([]);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div className="projects-section">
       <div className="projects-hero">
@@ -15,54 +33,18 @@ function ProjectsList() {
         </p>
       </div>
       <div className="projects-grid">
-        <div className="project-card d-flex jc-center al-center fd-column">
-          <div className="thumb tertiary-background"></div>
-          <h3>João Silva</h3>
-          <p>BH, Brasil</p>
-          <img src={LikedFilled} />
-        </div>
-        <div className="project-card d-flex jc-center al-center fd-column">
-          <div className="thumb tertiary-background"></div>
-          <h3>João Silva</h3>
-          <p>BH, Brasil</p>
-          <img src={LikedFilled} />
-        </div>
-        <div className="project-card d-flex jc-center al-center fd-column">
-          <div className="thumb tertiary-background"></div>
-          <h3>João Silva</h3>
-          <p>BH, Brasil</p>
-          <img src={LikedFilled} />
-        </div>
-        <div className="project-card d-flex jc-center al-center fd-column">
-          <div className="thumb tertiary-background"></div>
-          <h3>João Silva</h3>
-          <p>BH, Brasil</p>
-          <img src={LikedFilled} />
-        </div>
-        <div className="project-card d-flex jc-center al-center fd-column">
-          <div className="thumb tertiary-background"></div>
-          <h3>João Silva</h3>
-          <p>BH, Brasil</p>
-          <img src={LikedFilled} />
-        </div>
-        <div className="project-card d-flex jc-center al-center fd-column">
-          <div className="thumb tertiary-background"></div>
-          <h3>João Silva</h3>
-          <p>BH, Brasil</p>
-          <img src={LikedFilled} />
-        </div>
-        <div className="project-card d-flex jc-center al-center fd-column">
-          <div className="thumb tertiary-background"></div>
-          <h3>João Silva</h3>
-          <p>BH, Brasil</p>
-          <img src={LikedFilled} />
-        </div>
-        <div className="project-card d-flex jc-center al-center fd-column">
-          <div className="thumb tertiary-background"></div>
-          <h3>João Silva</h3>
-          <p>BH, Brasil</p>
-          <img src={LikedFilled} />
-        </div>
+        {projects.map((project) => (
+          <div
+            key={project.id}
+            className="project-card d-flex jc-center al-center fd-column">
+            <div
+              className="thumb tertiary-background"
+              style={{ backgroundImage: `url(${project.thumb})` }}></div>
+            <h3>{project.title}</h3>
+            <p>{project.subtitle}</p>
+            <img src={LikedFilled} height="20px" />
+          </div>
+        ))}
       </div>
     </div>
   );
