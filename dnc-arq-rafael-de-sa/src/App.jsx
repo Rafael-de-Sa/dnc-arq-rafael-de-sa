2ab003c3913cd46f2a88e62a1c185a1bb095e7fc
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-02-26 21:06:15 -0300

    ✨ feat: ScrollToTop on page change/reload

--- dnc-arq-rafael-de-sa/src/App.jsx
+++ dnc-arq-rafael-de-sa/src/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
-//Pages
+//PAGES
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
+//UTILS
+import ScrollToTop from "./utils/ScrollToTop";
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
