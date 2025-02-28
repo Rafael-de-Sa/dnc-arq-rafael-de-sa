45a67ade13978e3c4d71c41c6db54aa81dfa8233
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-02-28 09:07:06 -0300

    ✨ feat: basic form validation

--- dnc-arq-rafael-de-sa/src/components/ContactForm/ContactForm.jsx
+++ dnc-arq-rafael-de-sa/src/components/ContactForm/ContactForm.jsx
@@ -3,6 +3,43 @@ import "./ContactForm.css";
 import Button from "../Button/Button";
 
 function ContactForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (isFormValid) {
+      null;
+    }
+  };
+
+  useEffect(() => {
+    const isValidEmail = (email) => {
+      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      return emailRegex.test(email);
+    };
+
+    const isValid =
+      formData.name.trim() &&
+      formData.email.trim() &&
+      isValidEmail(formData.email) &&
+      formData.message.trim()
+    setIsFormValid(isValid);
+  }, [formData]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
     <div className="contact-form d-flex fd-column al-center">
       <h2>We love meeting new people and helping them.</h2>
@@ -14,6 +51,7 @@ function ContactForm() {
             id="name"
             name="name"
             placeholder="Name *"
+            onChange={handleChange}
           />
           <input
             type="email"
@@ -21,6 +59,7 @@ function ContactForm() {
             id="email"
             name="email"
             placeholder="Email *"
+            onChange={handleChange}
           />
         </div>
         <div className="d-flex form-group">
@@ -29,10 +68,11 @@ function ContactForm() {
             name="message"
             id="message"
             placeholder="Mensagem *"
+            onChange={handleChange}
             rows={4}></textarea>
         </div>
         <div className="al-center d-flex jc-end form-group">
-          <Button buttonStyle="secondary" type="submit">
+          <Button buttonStyle="secondary" type="submit" disabled={!isFormValid}>
             Send Now
           </Button>
         </div>
